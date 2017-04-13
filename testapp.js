66f8ae00042640073cb9a3a40ef5625f2eb9a16d
kiyeoneee <kyeoneee@gmail.com>
2017-04-13 13:44:17 +0000

    Change some code to solve encoding problem. (Make main function and do unicode encoding argument string data.)

--- testapp.js
+++ testapp.js
@@ -2,27 +2,6 @@
 var express = require('express');
 var app = express();
 
-var testData = '{"request" : ["spacing"], "spacingData" : "나는배가많이부른개발자야!", "modifiedData" : "aaaaaaaaaa"}'
-var jsonData = JSON.parse(testData);
-
-jsonData.request.forEach(function(e){
-	if(e == "spacing") {
-		console.log(jsonData.spacingData);
-		var spawn = require('child_process').spawn;
-		var py = spawn('python', ["/root/Okey_server/test/4_result.py", jsonData.spacingData]);
-		py.stdout.on('data', function (data) {
-			console.log(data.toString());
-		});		
-		py.stdout.on('exit', function (code) {
-			if (code != 0) {
-				console.log('Failed: ' + code);
-			}
-		});
-	}
-	else if(e == "modified")
-		console.log(e);
-});
-
 app.get('/', function(req, res){
         res.send('Okey Server');
 });
@@ -33,26 +12,49 @@ var server = net.createServer(function(client) {
 	console.log('Client connection: ');
 	console.log('   local = %s:%s', client.localAddress, client.localPort);
 	console.log('   remote = %s:%s', client.remoteAddress, client.remotePort);
-	client.setTimeout(3000);
+	//client.setTimeout(3000);
 	client.setEncoding('utf8');
 	client.on('data', function(data) {
   		console.log('Received data from client on port %d: %s', client.remotePort, data.toString());
 		console.log('  Bytes received: ' + client.bytesRead);
-		writeData(client, 'Sending: ' + data.toString() + '\n');
-		console.log('Sending: ' + data.toString() + '\n');
-		console.log('  Bytes sent: ' + client.bytesWritten);
+
+		var jsonData = JSON.parse(data);
+		console.log('jsonData : ' + jsonData.spacingData);
+		var spacedData = "";
+		jsonData.request.forEach(function(e){
+			if(e == "spacing") {
+				console.log('spacing start! : ' + jsonData.spacingData);
+				var spawn = require('child_process').spawn;
+				var py = spawn('python', ["test/4_result.py", jsonData.spacingData]);
+				py.stdout.on('data', function (data) {
+					spacedData += data;
+					spacedData = spacedData.trim();
+					console.log('After spacing : ' + spacedData)
+					var responseStr = '{ "response" : ["spacing"], "spacing" : "' + spacedData + '"}\n';
+					writeData(client, responseStr);
+					console.log('Sending: ' + responseStr);
+					console.log('  Bytes sent: ' + client.bytesWritten);
+				});		
+				py.stdout.on('exit', function (code) {
+					if (code != 0) {
+						console.log('Failed: ' + code);
+					}
+				});
+			}
+			else if(e == "modified")
+			console.log(e);
+		});
 	});
+  	
 	client.on('end', function() {
 		console.log('Client disconnected');
-		server.getConnections(function(err, count){
+		server.getConnections(function(err, count) {
 			console.log('Remaining Connections: ' + count);
 		});
 	});
-  
 	client.on('error', function(err) {
 		console.log('Socket Error: ', JSON.stringify(err));
 	});
-  
 	client.on('timeout', function() {
 		console.log('Socket Timed out');
 	});
