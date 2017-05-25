b8f307a23f8ce89f637a7954f4815f84a730bece
kiyeoneee <kyeoneee@gmail.com>
2017-05-26 02:01:56 +0900

    final

--- serverApp.js
+++ serverApp.js
@@ -7,7 +7,7 @@ var readline = require('readline');
 var isModify = false;
 var isSpace = false;
 var isGetMessageDone = false;
-
+var count = 0;
 
 var jsonData = "";
 var resultJson = "";
@@ -20,7 +20,6 @@ var options = {
 }
 
 var pyShell = new PythonShell("test.py", options);
-
 
 var server = net.createServer(function(client) {
 	console.log('Client connection: ');
@@ -42,49 +41,24 @@ var server = net.createServer(function(client) {
 		jsonData = JSON.parse(data);
 		resultJson = "";
 		modiData = "";		
-		//jsonData.request.forEach(function(e)
+	
 		if(jsonData.request[0] == "spacing") {
+			console.log('spacing');
 			pyShell.send('2' + jsonData.spacingData);
 			isModify = false;
 			isSpace = true;
+			writeData(client);
 		}
 		else if(jsonData.request[0] == "modified") {
+			console.log('modify');
 			pyShell.send('1'+jsonData.modifiedData.trim());
 			modiData = jsonData.modifiedData.trim();
 			isModify = true;
 			isSpace = false;
+			writeData(client);
 		}     
 	});
 
-	pyShell.on('message', function(message) {
-		if(isSpace) {
-			console.log('spacing end : ' + message);
-    
-			resultJson = ('"spacing" : "' + message + '"');
-			console.log('spacing end! : ', resultJson);
-			var resultResponse = responseStr + '"spacing"' + '], "spacing" : "' + message  + '"}\n\f\n';
-
-			console.log(resultResponse);
-			writeData(client, resultResponse);
-		}
-		if(isModify) {
-			console.log('modifying end : ' + message);
-			var modiAry = modiData.split(' ');
-
-			var resultAry = message.split(',');
-			resultJson = '"modified" : {'
-			for(var i = 0; i < modiAry.length; i++) {
-				if(i != 0) resultJson += ', '
-
-				resultJson += ('"' + modiAry[i] + '" : [ "' + i + '", "' + resultAry[i] +'"]');
-			}
-			resultJson += '}';
-			var resultResponse = responseStr + '"modified"' + '], ' + resultJson  + '}\n\f\n';
-			console.log(resultResponse);
-			writeData(client, resultResponse);
-		}
-	}); 
-  	
 	client.on('end', function() {
 		console.log('Client disconnected');
 		server.getConnections(function(err, count) {
@@ -109,14 +83,45 @@ server.listen(8100, function() {
 	});
 });
  
-function writeData(socket, data){
-	var success = !socket.write(data);
-	if (!success){
-		(function(socket, data){
-			socket.once('drain', function(){
-				writeData(socket, data);
-			});
-		})(socket, data);
-	}
+function writeData(client){
+	pyShell.on('message', function(message) {
+		if(isSpace) {
+			console.log('spacing end : ' + message);
+    
+			resultJson = ('"spacing" : "' + message + '"');
+			console.log('spacing end! : ', resultJson);
+			var resultResponse = responseStr + '"spacing"' + '], "spacing" : "' + message  + '"}\n\f\n';
+
+			console.log(resultResponse);
+			client.write(resultResponse);
+		}
+		if(isModify) {
+			console.log('modifying end : ' + message);
+			var modiAry = modiData.split(' ');
+
+			var resultAry = message.split(',');
+			resultJson = '"modified" : {'
+			for(var i = 0; i < modiAry.length; i++) {
+				if(i != 0) resultJson += ', '
+
+				resultJson += ('"' + modiAry[i] + '" : [ "' + i + '", "' + resultAry[i] +'"]');
+			}
+			resultJson += '}';
+			var resultResponse = responseStr + '"modified"' + '], ' + resultJson  + '}\n\f\n';
+			console.log(resultResponse);
+			client.write(resultResponse);
+		}
+	});
+
+	//var success = !client.write(data);
+//	pyShell.end();
+
+	//if (!success){
+	//	(function(socket, data){
+	//		socket.once('drain', function(){
+	//			writeData(socket, data);
+	//		});
+	//	})(socket, data);
+	//}
 }
 
